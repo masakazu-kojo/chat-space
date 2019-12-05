c95b7a5a6ed3894850d564dd3b17e63a97e09f9f
masakazu-kojo <2015ambition1112@gmail.com>
2019-12-05 20:17:46 +0900

    side-bat

--- config/routes.rb
+++ config/routes.rb
@@ -1,6 +1,8 @@
 Rails.application.routes.draw do
   devise_for :users
-  root to: "messages#index"
+  root 'groups#index'
   resources :users, only: [:edit, :update]
-  # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
+  resources :groups, only: [:new, :create, :edit, :update] do
+    resources :messages, only: [:index]
+  end
 end
